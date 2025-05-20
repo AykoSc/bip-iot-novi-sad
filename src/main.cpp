65725e4cef122f71392137958a5c75d4b7c8640a
Ayko <ayko.schwedler@studium.uni-hamburg.de>
2025-05-20 15:10:21 +0200

    Update diagram and code for Reaction Game, replacing buzzer and adjusting layout

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x3F or 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x3F for physical Box or 0x27 for virtual Wokwi
+
 
 // BIPBox pin assignments
 #define KEY1 39
@@ -28,16 +29,16 @@ enum gameState
   st_score
 };
 
-enum gameState state = st_waiting;
-unsigned long mils_start;
-unsigned long mils_reaction;
+gameState state = st_idle;
+unsigned long mils_start_reaction_signal;
+unsigned long mils_reaction_stimulus_shown;
 
 // variables for keys
-bool b1, b2, oldb1 = false, oldb2 = false, k1 = false, k2 = false;
+bool b1, b2, oldb1 = false, oldb2 = false, k1_pressed_event = false, k2_pressed_event = false;
 
 // Method forward declarations
 void keyLoop();
-bool readK1();
+bool consumeK1PressEvent();
 void scoreLoop();
 void reactionLoop();
 void startingLoop();
@@ -49,39 +50,48 @@ void setup()
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("BIPBox Reaction Speed Test Game\nWhen the buzzer sounds and the LED lights up, press the left key.");
+
   // keys
   pinMode(KEY1, INPUT); // Pullup resistors on the PCB, no need for INPUT_PULLUP
   pinMode(KEY2, INPUT); // and pullup resistors don't exist on pins 36 and 39 anyway
+
   // buzzer
-  pinMode(33, OUTPUT);
-  tone(33, 800, 250); // beep
-  // Print a message to the LCD.s
+  pinMode(BUZZER, OUTPUT);
+  tone(BUZZER, 800, 250); // beep
+
+  // Print a message to the LCD
   lcd.init(); // initialize the lcd
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Reaction Game");
-  lcd.setCursor(0, 1);
-  lcd.print("Wait for sound");
+
+  pinMode(LED1, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED3, LOW);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
   keyLoop();
+
   switch (state)
   {
   case st_idle:
     idleLoop();
     break;
+
   case st_waiting:
     waitingLoop();
     break;
+
   case st_starting:
     startingLoop();
     break;
+
   case st_reaction:
     reactionLoop();
     break;
+
   case st_score:
     scoreLoop();
     break;
@@ -93,100 +103,131 @@ void keyLoop()
   // ---- KEYS ----
   b1 = !digitalRead(KEY1);
   b2 = !digitalRead(KEY2);
-  if (b1 && (b1 != oldb1))
-    k1 = true;
-  else
-    k1 = false;
-  if (b2 && (b2 != oldb2))
-    k2 = true;
-  else
-    k2 = false;
+
+  // Check for a press event for KEY1
+  if (b1 && !oldb1)
+  {
+    k1_pressed_event = true;
+  }
+  // Check for a press event for KEY2
+  if (b2 && !oldb2)
+  {
+    k2_pressed_event = true;
+  }
   oldb1 = b1;
   oldb2 = b2;
 }
 
-bool readK1()
+// Call this to check if K1 was pressed SINCE THE LAST CALL TO THIS FUNCTION.
+// It consumes the event.
+bool consumeK1PressEvent()
 {
-  if (k1)
+  if (k1_pressed_event)
   {
-    k1 = false;
+    k1_pressed_event = false; // Consume
     return true;
   }
-  else
-  {
-    return false;
-  }
+  return false;
 }
 
 void scoreLoop()
 {
-  if (readK1())
+  if (consumeK1PressEvent())
   {
-    lcd.clear();
-    delay(250);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED3, LOW);
     state = st_idle;
   }
 }
 
 void reactionLoop()
 {
-  if (readK1())
+  if (consumeK1PressEvent())
   {
-    unsigned long mils_now = millis();
-    unsigned long score = mils_now - mils_reaction;
+    const unsigned long mils_now = millis();
+    const unsigned long score = mils_now - mils_reaction_stimulus_shown;
+    digitalWrite(LED3, LOW); // Turn off reaction LED
+
     lcd.clear();
     lcd.print("Reaction time:");
-    lcd.setCursor(2, 1);
+    lcd.setCursor(0, 1);
     lcd.print(score);
-    delay(750);
-    lcd.setCursor(9, 1);
-    lcd.print("Press A");
+    lcd.print(" ms");
+
+    // Display score for a bit, then prompt
+    delay(1500);
+    lcd.setCursor(0, 1);
+    lcd.print("          PressA");
     state = st_score;
   }
 }
 
 void startingLoop()
 {
-  if (mils_start - millis() > 0)
+  static bool ledBlinkState = false;
+  static unsigned long lastBlinkMils = 0;
+
+  if (millis() < mils_start_reaction_signal)
   {
-    if (readK1())
+    if (consumeK1PressEvent())
     {
-      // pressed the key too early - fail!
+      // Pressed the key too early - fail!
+      digitalWrite(LED1, LOW); // turn off the blinking LED
       lcd.clear();
       lcd.print("Too early!");
       lcd.setCursor(0, 1);
       lcd.print("--FAIL--");
-      tone(33, 300, 500);
-      delay(500);
+      tone(BUZZER, 300, 500);
+      delay(1000); // Increased delay to make "Too early" more visible
       state = st_score;
       return;
     }
-    else
+
+    // Blink LED1 while waiting
+    if (millis() - lastBlinkMils > 200)
     {
-      return;
+      ledBlinkState = !ledBlinkState;
+      digitalWrite(LED1, ledBlinkState);
+      lastBlinkMils = millis();
     }
+    return;
   }
-  tone(33, 800, 250);
-  mils_reaction = millis();
+
+  // --- Time for reaction signal! ---
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED3, HIGH);
+  tone(BUZZER, 800, 250);
+  mils_reaction_stimulus_shown = millis();
+  lcd.clear();
+  lcd.print("!!! PRESS A !!!");
+
   state = st_reaction;
 }
 
 void waitingLoop()
 {
-  if (readK1())
+  if (consumeK1PressEvent())
   {
     lcd.clear();
-    lcd.print("Press A on sound!");
+    lcd.print("Get Ready...");
+    lcd.setCursor(0, 1);
+    lcd.print("Press on sound!");
+
+    delay(1000);
+
+    mils_start_reaction_signal = millis() + random(2000, 5000);
+
+    digitalWrite(LED1, LOW);
     state = st_starting;
-    mils_start = millis() + random(1000, 5000); // pick the time 1s to 5s from current
-    // INTRODUCE SMALL DELAY TO ACCOUNT FOR KEY BOUNCE!
-    delay(100);
   }
 }
 
 void idleLoop()
 {
   lcd.clear();
+  lcd.print("Reaction Game");
+  lcd.setCursor(0, 1);
   lcd.print("Push A to start");
+
   state = st_waiting;
 }
